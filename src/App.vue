7391925f062d8554232f898d26c0bf3b31cd4d27
gab-che <gabriellachessa1@gmail.com>
2023-02-16 16:32:11 +0100

    fetches projects via api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <script>
+  import axios from 'axios';
   export default{
-    
+    data(){
+      return{
+        backendUrl: 'http://localhost:8000',
+        posts: [],
+      }
+    },
+
+    methods: {
+      fetchProjects(){
+        axios.get(this.backendUrl + '/api/projects')
+          .then((resp) =>{
+            this.posts = resp.data;
+          })
+      }
+    },
+
+    mounted(){
+      this.fetchProjects();
+    }
   }
 </script>
 
