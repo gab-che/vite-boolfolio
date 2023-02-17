1e78dff6f2b92e077a28d4b7e9d7b6b477f88b8f
gab-che <gabriellachessa1@gmail.com>
2023-02-17 21:55:08 +0100

    adds show (needs fixing)

--- src/pages/projects/Show.vue
+++ src/pages/projects/Show.vue
@@ -4,18 +4,41 @@
         name: 'ProjectsShow',
         data(){
             return{
-                project: store.singleProject,
+                store,
+                url: store.backendUrl,
             }
         },
 
         mounted(){
-            fetchSingleProject();
+            fetchSingleProject(this.$route.params.id);
         }
     }
 </script>
 
 <template>
-    {{ project.name }}
+    <div class="container py-5">
+        <div class="mb-3 d-flex justify-content-between align-items-center">
+            <h2 class="mb-3">{{ store.singleProject.name }}</h2>
+            <h3><span class="badge text-bg-success">{{ store.singleProject?.type.name }}</span></h3>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <p class="lead">{{ store.singleProject.description }}</p>
+            </div>
+            <div class="col-md-4">
+                <div v-if="store.singleProject.cover_img">
+                    <img :src="url + '/storage/' + store.singleProject.cover_img" :alt="store.singleProject.name" class="img-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 d-flex gap-2">
+                <h6 v-for="tag in store.singleProject?.technologies">
+                    <span class="badge text-bg-light">#{{ tag.name }}</span>
+                </h6>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
